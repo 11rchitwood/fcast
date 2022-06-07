4c14a8c262d62e36235da53aeffbf696b295847b
Ryan Chitwood <26073857+11rchitwood@users.noreply.github.com>
2022-06-07 12:59:42 -0400

    Style app.R

--- app.R
+++ app.R
@@ -2,38 +2,38 @@ library(shiny)
 library(forecast)
 
 ui <- shinyUI(
-    fluidPage(
-        titlePanel("fcast"),
-        sidebarLayout(
-            sidebarPanel(
-                selectInput(
-                    inputId = "dataset",
-                    label = "Dataset:",
-                    choices = c("AirPassengers", "WWWusage")
-                ),
-                numericInput(
-                    inputId = "h",
-                    label = "Forecast horizon:",
-                    value = 10
-                ),
-                checkboxInput(
-                    inputId = "box_cox",
-                    label = "Box-Cox",
-                )
-            ),
-            mainPanel(
-                plotOutput("forecastPlot")
-            )
+  fluidPage(
+    titlePanel("fcast"),
+    sidebarLayout(
+      sidebarPanel(
+        selectInput(
+          inputId = "dataset",
+          label = "Dataset:",
+          choices = c("AirPassengers", "WWWusage")
+        ),
+        numericInput(
+          inputId = "h",
+          label = "Forecast horizon:",
+          value = 10
+        ),
+        checkboxInput(
+          inputId = "box_cox",
+          label = "Box-Cox",
         )
+      ),
+      mainPanel(
+        plotOutput("forecastPlot")
+      )
     )
+  )
 )
 
 server <- shinyServer(function(input, output) {
-    output$forecastPlot <- renderPlot({
-        get(input$dataset) |>
-            forecast(h = input$h, lambda = if (input$box_cox) "auto") |>
-            plot()
-    })
+  output$forecastPlot <- renderPlot({
+    get(input$dataset) |>
+      forecast(h = input$h, lambda = if (input$box_cox) "auto") |>
+      plot()
+  })
 })
 
 shinyApp(ui = ui, server = server)
